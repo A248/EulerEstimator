5adb0e242a6d2d4ef92be2e498ba23f4313c5a62
A248 <anandebeh@gmail.com>
2019-11-17 17:00:56 -0500

    Add LICENSE header

--- Estimator/src/space/arim/estimator/EulerEstimator.java
+++ Estimator/src/space/arim/estimator/EulerEstimator.java
@@ -1,3 +1,21 @@
+/* 
+ * EulerEstimator
+ * Copyright © 2019 Anand Beh <https://www.arim.space>
+ * 
+ * EulerEstimator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EulerEstimator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with EulerEstimator. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
 package space.arim.estimator;
 
 import java.io.PrintStream;
